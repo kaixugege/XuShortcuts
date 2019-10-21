2c64d6ae4d85e33db4ce50962684edb77757bbc3
kaixugege <604312074@qq.com>
2019-10-21 17:40:41 +0800

    funk

--- app/src/main/java/com/xugege/xushortcuts/MainActivity.kt
+++ app/src/main/java/com/xugege/xushortcuts/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.xugege.xushortcuts
 
+import android.content.pm.ShortcutManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.core.content.getSystemService
 
 /**
- *  Shortcuts
+ *  Shortcuts 分动态和静态，因鄙人嫌弃静态的，所以这里只用动态的做示范。
  */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 只能在安卓7之后试用
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+            getSystemService(ShortcutManager::class.java)
+        };
+
     }
+
+
 }
