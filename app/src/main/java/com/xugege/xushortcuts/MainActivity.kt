1625d026afc7b5f949c3db1d443eec7b32aca167
kaixugege <604312074@qq.com>
2019-10-21 14:20:20 +0800

    Initial commit

---
+++ app/src/main/java/com/xugege/xushortcuts/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.xugege.xushortcuts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ *  Shortcuts
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
