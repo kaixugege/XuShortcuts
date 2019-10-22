e04ed82b7e0c3c9938e1d6f05c7c15adfbd95c82
kaixugege <604312074@qq.com>
2019-10-22 15:30:19 +0800

    funk

--- app/src/main/java/com/xugege/xushortcuts/MainActivity.kt
+++ app/src/main/java/com/xugege/xushortcuts/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.content.pm.ShortcutInfo
 import android.content.pm.ShortcutManager
 import android.graphics.drawable.Icon
-import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -28,7 +27,6 @@ class MainActivity : AppCompatActivity() {
             var intentThree = Intent(this,ThreeActivity::class.java)
             intentThree.setAction(Intent.ACTION_VIEW);
             var intentArray = arrayOf(intentThree,intent)
-
 
             var shortcutManager = getSystemService(ShortcutManager::class.java)
             var shortcut = ShortcutInfo.Builder(this, "xugege")
